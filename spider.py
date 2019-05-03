be3c0678af64f231011c04422e9a19a8305f2e95
__KK <44560179+PythonerKK@users.noreply.github.com>
2019-05-03 22:09:28 +0800

    Update spider.py

--- spider.py
+++ spider.py
@@ -10,7 +10,7 @@ from models import create_session, Comments
 class CommentFetcher:
 
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'}
-    cookie = ''
+    cookie = 'll="118281"; bid=JW9ceOtkzVQ; __utmc=30149280; __utmz=30149280.1556887217.1.1.utmcsr=baidu|utmccn=(organic)|utmcmd=organic; ap_v=0,6.0; __yadk_uid=4Qc0m9Yphgw7SrHQHicZBbM269AUCowI; push_noty_num=0; push_doumail_num=0; __utmv=30149280.19592; _pk_ref.100001.8cb4=%5B%22%22%2C%22%22%2C1556890576%2C%22https%3A%2F%2Fwww.baidu.com%2Flink%3Furl%3DfBPHI-eF--4C1vgnaxPkJOAHIFxxo7osPOKBVz_FwSO%26wd%3D%26eqid%3Dc27dab0d00213fbd000000065ccc36a5%22%5D; _pk_ses.100001.8cb4=*; __utma=30149280.1933236491.1556887217.1556887217.1556890577.2; _vwo_uuid_v2=D6231F23D873FE21CE33FE82E12899FB3|0b57003a81123ce8df6e42ca951de47f; __utmt=1; dbcl2="195925767:zfIHw/L/COw"; ck=qHHm; _pk_id.100001.8cb4=cd248ea63d8d938d.1556887213.2.1556891683.1556887266.; __gads=ID=91c2f88c9eec95d1:T=1556891681:S=ALNI_MZDvckmgPiDRE9BrmUOq-pxtctaIw; __utmb=30149280.8.10.1556890577'
     cookies = {'cookie': cookie}
     base_node = '//div[@class="comment-item"]'
     #cookie为登录后的cookie，需要自行复制
@@ -32,52 +32,67 @@ class CommentFetcher:
         self.session = create_session()
 
     def _get(self):
-        res = requests.get(self.url, cookies=self.cookies, headers=self.headers)
-        res = res.json()['html']
+        res = ''
+        try:
+            res = requests.get(self.url, cookies=self.cookies, headers=self.headers)
+            res = res.json()['html']
+        except Exception as e:
+            print('IP被封，请使用代理IP')
         print('正在获取{} 开始的记录'.format(self.start))
         return res
 
     def _parse(self):
         res = self._get()
         dom = etree.HTML(res)
-        self.id = dom.xpath(self.base_node + '/@data-cid')
-        self.username = dom.xpath(self.base_node + '/div[@class="avatar"]/a/@title')
-        self.user_center = dom.xpath(self.base_node + '/div[@class="avatar"]/a/@href')
 
+        self.id = dom.xpath(self.base_node + '/@data-cid')
+        #用户名
+        self.username = dom.xpath(self.base_node + '/div[@class="avatar"]/a/@title')
+        #用户连接
+        self.user_center = dom.xpath(self.base_node + '/div[@class="avatar"]/a/@href')
+        #点赞数
         self.vote = dom.xpath(self.base_node + '//span[@class="votes"]/text()')
+        #星级
         self.star = dom.xpath(self.base_node + '//span[contains(@class,"rating")]/@title')
+        #发表时间
         self.time = dom.xpath(self.base_node + '//span[@class="comment-time "]/@title')
+        #评论内容
         self.content = dom.xpath(self.base_node + '//span[@class="short"]/text()')
 
 
     def save(self):
         self._parse()
-        try:
-            comments = [Comments(
-                id=int(self.id[i]),
-                username=self.username[i],
-                user_center=self.user_center[i],
-                vote=int(self.vote[i]),
-                star=self.star[i],
-                time=datetime.strptime(self.time[i], '%Y-%m-%d %H:%M:%S'),
-                content=self.content[i]
-            ) for i in range(20)]
-            self.session.add_all(comments)
-            self.session.commit()
+        #数据一条一条插入
+        for i in range(len(self.id)):
+            try:
+                comment = Comments(
+                    id=int(self.id[i]),
+                    username=self.username[i],
+                    user_center=self.user_center[i],
+                    vote=int(self.vote[i]),
+                    star=self.star[i],
+                    time=datetime.strptime(self.time[i], '%Y-%m-%d %H:%M:%S'),
+                    content=self.content[i]
+                )
 
-        except pymysql.err.IntegrityError as e:
-            pass
+                self.session.add(comment)
+                self.session.commit()
 
-        except Exception as e:
-            self.session.rollback()
+            except pymysql.err.IntegrityError as e:
+                print('数据重复，不做任何处理')
 
-        finally:
-            self.session.close()
+            except Exception as e:
+                print('包含特殊字符，数据库回滚')
+                self.session.rollback()
+
+            finally:
+                #关闭数据库连接
+                self.session.close()
 
 if __name__ == '__main__':
-    #最多爬取24页
-    for i in range(25):
-        fetcher = CommentFetcher(movie_id=26266893, start=i * 20, type='')
-        fetcher.save()
-
-
+    #爬取综合评论、好评、中评、差评
+    for i in ['', 'h', 'm', 'l']:
+        #最多爬取24页
+        for j in range(25):
+            fetcher = CommentFetcher(movie_id=26266893, start=j * 20, type=i)
+            fetcher.save()
